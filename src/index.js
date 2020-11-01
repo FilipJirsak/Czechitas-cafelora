fbb326c86b8a9d8e362a2ee4119fc4e60609bd6b
Filip Jirsák <filip@jirsak.org>
2020-11-01 13:47:21 +0100

    Úkol č. 3: Zprovoznění navigace.

--- src/index.js
+++ src/index.js
@@ -2,3 +2,10 @@ import './index.html';
 import './style.css';
 
 console.log('funguju!');
+
+const navbar = document.querySelector('nav');
+
+const closeNavbar = () => navbar.classList.toggle('nav-closed')
+
+document.querySelector('#nav-btn').addEventListener('click', closeNavbar)
+document.querySelectorAll('nav > a').forEach(navItem => navItem.addEventListener('click', closeNavbar))
