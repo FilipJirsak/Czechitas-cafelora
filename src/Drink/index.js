13f3937a4ab70a286f4e5586272791d32a10260d
Filip Jirsák <filip@jirsak.org>
2020-11-01 12:51:30 +0100

    Úkol č. 7: Nápoj jako komponenta.

---
+++ src/Drink/index.js
@@ -0,0 +1,49 @@
+import './style.css'
+import {Layer} from '../Layer/index.js'
+
+export const Drink = (props) => {
+    const drink = document.createElement('div');
+    drink.classList.add('drink');
+
+    // produkt
+    const drinkProduct = document.createElement('div');
+    drinkProduct.classList.add('drink__product');
+    drink.appendChild(drinkProduct);
+
+    const drinkCup = document.createElement('div');
+    drinkCup.classList.add('drink__cup');
+    drinkProduct.appendChild(drinkCup);
+
+    const img = document.createElement('img');
+    img.src = `/assets/cups/${props.id}.png`;
+    drinkCup.appendChild(img);
+
+    const drinkInfo = document.createElement('div');
+    drinkInfo.classList.add('drink__info');
+    drinkProduct.appendChild(drinkInfo);
+
+    const drinkInfoH3 = document.createElement('h3')
+    drinkInfoH3.textContent = props.name;
+    drinkInfo.appendChild(drinkInfoH3);
+
+    props.layers.forEach(layer => drinkInfo.innerHTML += Layer(layer));
+    
+    // objednávka
+    const drinkControls = document.createElement('div');
+    drinkControls.classList.add('drink__controls');
+    drink.appendChild(drinkControls);
+
+    const orderBtn = document.createElement('button');
+    orderBtn.classList.add('order-btn');
+    orderBtn.textContent = 'Objednat';
+    drinkControls.appendChild(orderBtn);
+
+    orderBtn.addEventListener('click', () => {
+        props.ordered = !props.ordered;
+        drinkCup.classList.toggle('drink__cup--selected', props.ordered);
+        orderBtn.textContent = props.ordered ? 'Zrušit' : 'Objednat'
+    })
+   
+
+    return drink
+}
--- src/index.js
+++ src/index.js
@@ -10,30 +10,41 @@ const closeNavbar = () => navbar.classList.toggle('nav-closed')
 document.querySelector('#nav-btn').addEventListener('click', closeNavbar)
 document.querySelectorAll('nav > a').forEach(navItem => navItem.addEventListener('click', closeNavbar))
 
-// objednávání
-const drinkCup = document.querySelector('.drink__cup');
-const orderBtn = document.querySelector('.order-btn');
-orderBtn.addEventListener('click', () => {
-    const selected = drinkCup.classList.toggle('drink__cup--selected');
-    orderBtn.textContent = selected ? 'Zrušit' : 'Objednat'
-})
-
-// seznam ingredience
-import {Layer} from './Layer/index.js'
-const layers = [
-    {
-      color: '#feeeca',
-      label: 'mléčná pěna',
-    },
-    {
-      color: '#fed7b0',
-      label: 'teplé mléko',
-    },
-    {
-      color: '#613916',
-      label: 'espresso',
-    },
-  ];
-layers.forEach(layer => {
-    document.querySelector('.drink__info').innerHTML += Layer(layer);
-})
+// seznam nápojů
+import { Drink } from './Drink/index.js'
+document.querySelector('.drinks-list').append(
+    Drink({
+        id: 'cappuccino',
+        name: 'Capuuccino',
+        ordered: false,
+        layers: [
+            {
+                color: '#feeeca',
+                label: 'mléčná pěna',
+            },
+            {
+                color: '#fed7b0',
+                label: 'teplé mléko',
+            },
+            {
+                color: '#613916',
+                label: 'espresso',
+            },
+        ]
+    }),
+    Drink({
+        id: 'romano',
+        name: 'Romano',
+        ordered: false,
+        layers: [
+            {
+                color: '#fbdf5b',
+                label: 'citrón',
+            },
+            {
+                color: '#613916',
+                label: 'espresso',
+            },
+        ]
+    }),
+);
