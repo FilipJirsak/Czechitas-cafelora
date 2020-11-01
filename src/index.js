afa1754e305f3971a9da37f89af23dd79c5d42b7
Filip Jirsák <filip@jirsak.org>
2020-11-01 13:24:13 +0100

    Úkol č. 5: Ingredience jako komponenta.

--- src/index.js
+++ src/index.js
@@ -17,3 +17,19 @@ orderBtn.addEventListener('click', () => {
     const selected = drinkCup.classList.toggle('drink__cup--selected');
     orderBtn.textContent = selected ? 'Zrušit' : 'Objednat'
 })
+
+// ingredience
+import {Layer} from './Layer/index.js'
+document.querySelector('.drink__info').innerHTML +=
+    Layer({
+        color: '#feeeca',
+        label: 'mléčná pěna'
+    })+
+    Layer({
+        color: '#fed7b0',
+        label: 'teplé mléko'
+    })+
+    Layer({
+        color: '#613916',
+        label: 'espresso'
+    });
