6080757db2d02efeed693f764a24507534323eda
Filip Jirsák <filip@jirsak.org>
2020-11-01 11:58:14 +0100

    Úkol č. 4: Objednávání.

--- src/index.js
+++ src/index.js
@@ -3,9 +3,17 @@ import './style.css';
 
 console.log('funguju!');
 
+// navigace
 const navbar = document.querySelector('nav');
-
 const closeNavbar = () => navbar.classList.toggle('nav-closed')
 
 document.querySelector('#nav-btn').addEventListener('click', closeNavbar)
 document.querySelectorAll('nav > a').forEach(navItem => navItem.addEventListener('click', closeNavbar))
+
+// objednávání
+const drinkCup = document.querySelector('.drink__cup');
+const orderBtn = document.querySelector('.order-btn');
+orderBtn.addEventListener('click', () => {
+    const selected = drinkCup.classList.toggle('drink__cup--selected');
+    orderBtn.textContent = selected ? 'Zrušit' : 'Objednat'
+})
