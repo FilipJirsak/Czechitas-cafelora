94d3afe12368090a379bf00bb744f2bc4fd5eb1e
Filip Jirsák <filip@jirsak.org>
2020-11-01 14:36:12 +0100

    Pro stažení seznamu kafí použít adresu s HTTPS.

--- src/index.js
+++ src/index.js
@@ -12,6 +12,6 @@ document.querySelectorAll('nav > a').forEach(navItem => navItem.addEventListener
 
 // seznam nápojů
 import { Drink } from './Drink/index.js'
-fetch('http://cafelora.kodim.cz/api/drinks')
+fetch('https://run.mocky.io/v3/920f04d5-bf2e-49b3-9402-c693e6ea9499')
     .then(response => response.json())
     .then(drinks => drinks.forEach(drink => document.querySelector('.drinks-list').append(Drink(drink))));
