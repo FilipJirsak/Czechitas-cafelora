873faf6c4547bbf6ec60b74cd814bbd46ef380d0
Filip Jirsák <filip@jirsak.org>
2022-04-30 11:11:46 +0200

    Přizpůsobení novému API

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import './index.html';
 import './style.css';
 
-console.log('funguju!');
-
 // navigace
 const navbar = document.querySelector('nav');
 const closeNavbar = () => navbar.classList.toggle('nav-closed')
@@ -13,5 +11,5 @@ document.querySelectorAll('nav > a').forEach(navItem => navItem.addEventListener
 // seznam nápojů
 import { Drink } from './Drink/index.js'
 fetch('https://apps.kodim.cz/daweb/cafelora/api/drinks')
-    .then(response => response.json())
+    .then(response => response.json().results)
     .then(drinks => drinks.forEach(drink => document.querySelector('.drinks-list').append(Drink(drink))));
